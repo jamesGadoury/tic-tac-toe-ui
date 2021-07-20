3661f931762ed093d5e2301f367c7501a7aa2fcd
James Gadoury <gadouryjames@gmail.com>
2021-07-19 23:45:45 -0400

    Calculate Winner functionality

--- src/index.js
+++ src/index.js
@@ -11,6 +11,26 @@ function Square(props) {
     );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -46,7 +66,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Current player's turn: ${this.state.currentPlayerMarker}`; 
+    const winner = calculateWinner(this.state.squares);
+    const status = ( winner ? 
+      `Winner: ${winner}` :
+      `Current player's turn: ${this.state.currentPlayerMarker}`
+    );
 
     return (
       <div>
