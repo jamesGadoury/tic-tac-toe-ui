17fb5563302566d398a39bf8726513fe5797afa1
James Gadoury <gadouryjames@gmail.com>
2021-07-21 22:13:51 -0400

    Give the project better file structure by moving components into their own separate files in a new folder.

---
+++ src/components/Board.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../index.css';
+import { Square } from './Square';
+
+function Board(props)  {
+    const renderSquare = (i) => {
+      return (
+        <Square 
+          value={props.squares[i]}
+          onClick={() => props.onClick(i)} 
+        />
+      ); // parantheses used so that Javascript doesn't insert a colon after return and break things
+    }
+  
+    return (
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+}
+
+export { Board };
