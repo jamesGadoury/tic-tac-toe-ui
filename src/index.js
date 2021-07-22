301bf79d0ebac7599cd23b71c6a34c164c928cb1
James Gadoury <gadouryjames@gmail.com>
2021-07-21 21:49:20 -0400

    Make Board a functional component

--- src/index.js
+++ src/index.js
@@ -11,37 +11,35 @@ function Square(props) {
     );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
+function Board(props)  {
+  const renderSquare = (i) => {
     return (
       <Square 
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)} 
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)} 
       />
     ); // parantheses used so that Javascript doesn't insert a colon after return and break things
   }
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 function calculateDraw(squares) {
