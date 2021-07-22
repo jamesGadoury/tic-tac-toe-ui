28e19f8122e1cdb7723d8a5d37a7958e89ece113
James Gadoury <gadouryjames@gmail.com>
2021-07-22 00:20:44 -0400

    Make Game a function component with hooks

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../index.css';
 import { Board } from './Board';
+
+const HUMAN_PLAYER_MARKER = 'X';
+const AI_PLAYER_MARKER    = 'O';
+
+function calculateWinner(squares) {
+  const LINES = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < LINES.length; i++) {
+    const [a, b, c] = LINES[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 function calculateDraw(squares) {
   for (const square of squares) {
@@ -11,27 +34,7 @@ function calculateDraw(squares) {
   return true;
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-function statusOfBoard(squares, currentPlayerMarker) {
+function statusOfBoard(squares) {
   const winner = calculateWinner(squares);
 
   if (winner) {
@@ -43,19 +46,7 @@ function statusOfBoard(squares, currentPlayerMarker) {
     return 'Draw';
   }
   
-  return `Current player's turn: ${currentPlayerMarker}`
-}
-
-function nextPlayerMarker(currentPlayerMarker) {
-  return currentPlayerMarker === 'X' ? 'O' : 'X';
-}
-
-function playerThisMove(move) {
-  return (move % 2) === 0 ? 'X' : 'O';
-}
-
-function currentPlayerIsAI(currentPlayerMarker) {
-  return currentPlayerMarker === 'O';
+  return `Current player's turn: ${currentPlayerMarker(squares)}`
 }
 
 function cornerWasPlayed(squares) {
@@ -72,14 +63,10 @@ function getRandomInt(min, max) {
 function randomCornerPlay(squares) {
   const corners = [0, 2, 6, 8];
   const availableCorners = corners.filter(corner => !squares[corner]);
-  if (availableCorners === undefined || availableCorners.length == 0) {
+  if (availableCorners === undefined || availableCorners.length === 0) {
     return null; // todo error would likely be best, but don't want to deal with that complexity at this stage
   }
   return corners[getRandomInt(0, availableCorners.length)]
-}
-
-function searchForBestPlay(squares) {
-  // todo
 }
 
 function generateRandomPlay(squares) {
@@ -89,15 +76,15 @@ function generateRandomPlay(squares) {
       availableSquares.push(i);
     }
   }
-  if (availableSquares === undefined || availableSquares.length == 0) {
+  if (availableSquares === undefined || availableSquares.length === 0) {
     return null; // todo error would likely be best
   }
   return availableSquares[getRandomInt(0, availableSquares.length)];
 }
 
-function generateAIPlay(squares, moveNumber) {
+function generateAIPlay(squares) {
   // three condition action rules based on AI always going second
-  if (moveNumber === 1) {
+  if (moveNumber(squares) === 1) {
     if (cornerWasPlayed(squares)) {
       return 4; // this is the middle square
     }
@@ -106,21 +93,28 @@ function generateAIPlay(squares, moveNumber) {
   return generateRandomPlay(squares);
 }
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      currentPlayerMarker: 'X',
-      moveNumber: 0
-    }
-  }
+function playerThisMove(move) {
+  return (move % 2) === 0 ? HUMAN_PLAYER_MARKER : AI_PLAYER_MARKER;
+}
+function moveNumber(squares) { // todo moveNumber bad name -> movesPlayed
+  return squares.filter(square => square).length;
+}
 
-  handleClick(i) {
-    const currentPlayerMarker = this.state.currentPlayerMarker;
-    const history = this.state.history.slice(0, this.state.moveNumber+1);
+function currentPlayerMarker(squares) {
+  return playerThisMove(moveNumber(squares)); // todo playerThisMove -> markerThisMove
+}
+
+function currentPlayerIsAI(squares) {
+  const moves = moveNumber(squares);
+  return (moves % 2 !== 0 && moves !== 0);
+}
+
+function Game() {
+  const [history, setHistory] = useState([{
+    squares: Array(9).fill(null),
+  }]);
+
+  const handleClick = (i) => {
     const current = history[history.length - 1];
 
     // replace data with a new copy, instead of mutating original data
@@ -129,101 +123,84 @@ class Game extends React.Component {
     // which helps to determine when a component requires re-rendering
     const squares = current.squares.slice();
 
-    if (calculateWinner(squares) || squares[i] || currentPlayerIsAI(currentPlayerMarker)) {
+    if (calculateWinner(squares) || squares[i] || currentPlayerIsAI(squares)) {
       // either there is already a winner, or someone is trying
       // to select a square that has already been picked, or
       // it isn't the player's turn
       return;
     }
+    squares[i] = currentPlayerMarker(squares);
 
-    squares[i] = currentPlayerMarker;
+    setHistory(history.concat([{
+      squares: squares
+    }]));
+  }
+  
+  useEffect(() => {
+    const handleAIPlay = (i) => {
+      const current = history[history.length - 1];
 
-    this.setState({
-      history: history.concat([{
-        squares: squares
-      }]),
-      currentPlayerMarker: nextPlayerMarker(currentPlayerMarker),
-      moveNumber: history.length,
-    }, () => {
-      if (!calculateWinner(squares) && !calculateDraw(squares)) {
-        this.handleAIPlay(generateAIPlay(squares, this.state.moveNumber)); // todo, really should be getting moveNumber from count of squares 
+      // replace data with a new copy, instead of mutating original data
+      // React apparently loves immutability - see `pure components`
+      // this helps React easily determine if changes have been made,
+      // which helps to determine when a component requires re-rendering
+      const squares = current.squares.slice();
+
+      if (!currentPlayerIsAI(squares)) {
+        console.log("handleAIPlay called while current player is human!!!");
       }
-    });
 
-  }
+      squares[i] = AI_PLAYER_MARKER;
 
-  handleAIPlay(i) {
-    const currentPlayerMarker = 'O';
-    const history = this.state.history.slice(0, this.state.moveNumber+1);
-    const current = history[history.length - 1];
-
-    // replace data with a new copy, instead of mutating original data
-    // React apparently loves immutability - see `pure components`
-    // this helps React easily determine if changes have been made,
-    // which helps to determine when a component requires re-rendering
-    const squares = current.squares.slice();
-    squares[i] = currentPlayerMarker;
-
-    this.setState({
-      history: history.concat([{
+      setHistory(history.concat([{
         squares: squares
-      }]),
-      currentPlayerMarker: nextPlayerMarker(currentPlayerMarker),
-      moveNumber: history.length,
-    });
+      }]));
+    }
+    
+    const squares = history[history.length-1].squares;
+    if (playerThisMove(moveNumber(squares)) === HUMAN_PLAYER_MARKER || 
+        calculateDraw(squares) || calculateWinner(squares)) {
+      return;
+    }
+    handleAIPlay(generateAIPlay(squares));
+  }, [history]);
+
+
+  const jumpTo = (move) => {
+    setHistory(history.slice(0, move));
   }
 
-  jumpTo(move) {
-    this.setState({
-      moveNumber: move,
-      currentPlayerMarker: playerThisMove(move)
-    },() => {
-      if (currentPlayerIsAI(this.state.currentPlayerMarker)) {
-        const moveNumber = this.state.moveNumber;
-        const history = this.state.history.slice(0, moveNumber+1);
-        const squares = history[history.length - 1].squares;
+  const squares = history[history.length-1].squares;
+  const status = statusOfBoard(squares); 
 
-        this.handleAIPlay(generateAIPlay(squares, moveNumber)); // todo, really should be getting moveNumber from count of squares 
-      } 
-    });
-  }
-
-  render() {
-    const currentPlayerMarker = this.state.currentPlayerMarker;
-    const history = this.state.history;
-    const current = history[this.state.moveNumber];
-    const squares = current.squares;
-    const status = statusOfBoard(squares, currentPlayerMarker); 
-
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        `Go to move #${move}` :
-        'Go to game start';
-
-      return (
-        // we add a key to give React the ability to know
-        // what components to update
-        <li key={move}> 
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
+  const moves = history.map((step, move) => {
+    const desc = move ?
+      `Go to move #${move}` :
+      'Go to game start';
 
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares={squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
+      // we add a key to give React the ability to know
+      // what components to update
+      <li key={move}> 
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
     );
-  }
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board 
+          squares={squares}
+          onClick={(i) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
 
 export { Game };
