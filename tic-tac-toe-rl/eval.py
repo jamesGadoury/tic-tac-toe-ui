cacefe8c931d1480b742dc359d737c379086f67e
James Gadoury <gadouryjames@gmail.com>
2025-07-27 02:39:57 -0400

    imple eval

---
+++ tic-tac-toe-rl/eval.py
@@ -0,0 +1,112 @@
+import json
+from argparse import ArgumentParser
+from dataclasses import asdict, dataclass
+from enum import IntEnum
+from math import exp
+from pathlib import Path
+from random import choice, seed
+from time import time_ns
+from typing import Any, Protocol
+
+from agents import Agent, QAgent, RandomAgent
+from tic_tac_toe import (
+    Board,
+    GameState,
+    Marker,
+    available_plays,
+    game_state,
+    new_board,
+    next_marker_to_place,
+    transition,
+)
+from tqdm import tqdm
+from train import find_most_recent_file_with_substring, reward_from_board_transition
+
+
+def load_q_agent(dir: Path) -> QAgent:
+    # NOTE: very hacky, have to search for canonical first because q_table will match both canonical and regular q tables
+    canonical_q_table_pth = find_most_recent_file_with_substring(
+        dir=Path(dir), substring="canonical_q_table"
+    )
+    assert canonical_q_table_pth is not None
+    q_table_pth = canonical_q_table_pth.with_name(
+        canonical_q_table_pth.name.replace("canonical_", "")
+    )
+
+    canonical_q_table = json.load(open(canonical_q_table_pth, "r"))
+    q_table = json.load(open(q_table_pth, "r"))
+
+    print(f"Loaded {canonical_q_table_pth}")
+    print(f"Loaded {q_table_pth}")
+
+    return QAgent.load(
+        canonical_q_table=canonical_q_table, q_table=q_table, frozen=False
+    )
+
+
+def eval(n_episodes: int, agents: dict[Marker, Agent]) -> list[float]:
+    rewards = []
+    for ep in tqdm(range(n_episodes)):
+        board: Board = new_board()
+        while True:
+            marker = next_marker_to_place(board)
+            action = agents[marker].get_action(state_t=board, epsilon=0.0)
+            board = transition(board, action)
+            reward = reward_from_board_transition(board)
+
+            if game_state(board) != GameState.INCOMPLETE:
+                rewards.append(reward if marker == Marker.FIRST_PLAYER else -reward)
+                break
+    return rewards
+
+
+def output_results(rewards: list[float]):
+    wins = sum([r == 1.0 for r in rewards])
+    ties = sum([r == 0.0 for r in rewards])
+    losses = sum([r == -1.0 for r in rewards])
+    total = wins + ties + losses
+
+    print(f"win_or_tie_ratio: {(wins+ties) / total}")
+    print(f"win_ratio: {wins / total}")
+    print(f"tie_ratio: {ties / total}")
+    print(f"loss_ratio: {losses/ total}")
+    assert total == len(rewards)
+
+
+def main(n_episodes: int, pretrained_dir: Path, random_seed: int):
+    seed(random_seed)
+
+    q_agent = load_q_agent(pretrained_dir)
+    random_agent = RandomAgent()
+
+    print("Q Agent as first player:")
+    output_results(
+        eval(
+            n_episodes=n_episodes,
+            agents={Marker.FIRST_PLAYER: q_agent, Marker.SECOND_PLAYER: random_agent},
+        )
+    )
+
+    print("Q Agent as second player:")
+    output_results(
+        eval(
+            n_episodes=n_episodes,
+            agents={Marker.FIRST_PLAYER: random_agent, Marker.SECOND_PLAYER: q_agent},
+        )
+    )
+
+
+if __name__ == "__main__":
+    cli = ArgumentParser()
+    cli.add_argument("--n-episodes", help="how many episodes", type=int, required=True)
+    # NOTE: have to save as str because json package doesn't support Path directly
+    cli.add_argument(
+        "--pretrained-dir", help="dir to load past training artifacts from", type=Path
+    )
+    cli.add_argument("--seed", help="random seed", type=int, default=42)
+    args = cli.parse_args()
+    main(
+        n_episodes=args.n_episodes,
+        pretrained_dir=args.pretrained_dir,
+        random_seed=args.seed,
+    )
